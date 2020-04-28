144ea0d6f13ab367b2400511f4e0710e5a9cee96
vagrant <j_khabra11@hotmail.com>
2020-04-28 21:38:15 +0000

    Queries 4

---
+++ 4_queries/10_most_confusing_assign.sql
@@ -0,0 +1,10 @@
+SELECT 
+  assignments.id as id,
+  assignments.name as name,
+  assignments.chapter as chapter,
+  assignments.day as day,
+  count(assistance_requests.*) as total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assignment_id
+GROUP BY assignments.id 
+ORDER BY total_requests DESC;
